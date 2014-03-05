ab9718fbab4b3bbe305e7a1f9670d094ac36e178
iambernie <iambernie@gmail.com>
2014-03-05 21:07:45 +0100

    [non-commit] transfer only

--- binary_system.py
+++ binary_system.py
@@ -10,30 +10,55 @@ from amuse.units import units
 from amuse.datamodel.particles import Particles
 from amuse.community.hermite0.interface import Hermite
 from amuse.units.nbody_system import nbody_to_si
+from amuse.units.quantities import AdaptingVectorQuantity
+
+from hdf5support import write_to_hdf5
+
+import matplotlib.pyplot as plt
 
 def main():
+    data = evolve_system()
+    write_to_hdf5(args.write, data)
+
+    plot_energy(data['times'], data['energies'])
+    #plot_positions(data)
+    
+
+def evolve_system():
     """
     Integrates the orbit of a binary system.
-    
     """
-
     #for now define here, but use argparse later.
     n_steps = 1000
-    t_end = 100 | units.yr
+    t_end = 10000 | units.yr
     masses = [10, 1] | units.MSun
     positions = [[1,0,0], [0,0,0]] | units.AU
-    velocities = [[0, 10, 0], [0, -10, 0]] | units.kms
+    velocities = [[0, 1000, 0], [0, -1000, 0]] | units.kms
  
     bodies = binary_system(masses, positions, velocities)
 
     integrator = Hermite(nbody_to_si(bodies.total_mass(), 10.0 | units.AU))
     integrator.particles.add_particles(bodies)
 
+    positions_in_time = AdaptingVectorQuantity()
+    velocities_in_time = AdaptingVectorQuantity()
+    energy_in_time = AdaptingVectorQuantity()
+    
     times = linspace(1|units.yr, t_end, n_steps)
     for t in times:
+        print(t.value_in(units.yr))
         integrator.evolve_model(t)
-   
-    return 0 
+        positions_in_time.append(integrator.particles.position)
+        velocities_in_time.append(integrator.particles.velocity)
+        energy_in_time.append(integrator.get_total_energy())
+
+    integrator.stop()
+
+    data = dict(positions=positions_in_time,
+                velocities=velocities_in_time,
+                energies=energy_in_time,
+                times=times)
+    return data 
 
 def binary_system(masses, positions, velocities):
     """
@@ -57,46 +82,52 @@ def binary_system(masses, positions, velocities):
 
     return bodies
     
-def plot_sma_vs_timestep():
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    ax.set_xlabel('delta T/Period')
-    ax.set_ylabel('A/A0')
-    ax.set_title('Semi-major-axis vs timestep')
-    ax.plot(x, y)
-
-    #ax.set_xlim(10,30)
-    #ax.set_ylim(10,30)
-    #ax1.set_xticklabels([]) #removes ticklables
-    #ax1.set_xticks()    
-
-    plt.savefig('test.png', bbox_inches='tight', dpi=150)
-    fig.clf()
-    plt.close()
-
-
-def plot_energy():
-
+def plot_energy(x, y):
+    """
+    x: time
+    y: energy
+    """
+    
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     ax.set_xlabel('time')
     ax.set_ylabel('total energy')
-    ax.plot(x, y)
+    ax.plot(x.value_in(units.yr), y.value_in(units.J))
 
     #ax.set_xlim(10,30)
     #ax.set_ylim(10,30)
     #ax1.set_xticklabels([]) #removes ticklables
     #ax1.set_xticks()    
 
-    plt.savefig('positions.png', bbox_inches='tight', dpi=150)
+    plt.savefig('total_energy.png', bbox_inches='tight', dpi=150)
     fig.clf()
     plt.close()
 
-def plot_positions():
-    pass
+def plot_positions(data):
+    """
+    pos1
+    """
+
+    data['positions']
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.plot(x1.value_in(units.AU), y1.value_in(units.AU))
+    ax.plot(x2.value_in(units.AU), y2.value_in(units.AU))
+
+    #ax.set_xlim(10,30)
+    #ax.set_ylim(10,30)
+    #ax1.set_xticklabels([]) #removes ticklables
+    #ax1.set_xticks()    
+
+    plt.savefig('total_energy.png', bbox_inches='tight', dpi=150)
+    fig.clf()
+    plt.close()
 
 def plot_velocities():
     pass
@@ -113,7 +144,8 @@ def get_options():
     #               help='Increases verbosity.')
     #parser.add_argument('-o','--outputdir', type=str, default='images',\
     #               help='output directory for images.')
-
+    parser.add_argument('-w','--write', type=str, default='binary.hdf5',\
+                   help='Filepath to write hdf5 file to.')
     #parser.add_argument('-t','--test',\
     #               action='store_true',\
     #               help='Use test sequence [2,2,2,1,2].')
