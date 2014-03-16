d2f2eed63fccea160482fe16f1acb5d7dd99b0a9
iambernie <iambernie@gmail.com>
2014-03-16 16:32:35 +0100

    added twobodies_circular to systems.py

--- systems.py
+++ systems.py
@@ -1,6 +1,8 @@
-from amuse.units import units
-from amuse.datamodel.particles import Particles
+import numpy
 
+from amuse.units import units
+from amuse.units.quantities import AdaptingVectorQuantity
+from amuse.datamodel.particles import Particles
 
 def sun_and_earth():
     """
@@ -11,21 +13,57 @@ def sun_and_earth():
     Particles instance representing the sun and earth.
 
     """
-    stars = Particles(2)
+    bodies = Particles(2)
 
-    sun = stars[0]
+    sun = bodies[0]
     sun.mass = 1.0 | units.MSun
     sun.position = (0.0, 0.0, 0.0) | units.m
     sun.velocity = (0.0, 0.0, 0.0) | (units.m / units.s)
     sun.radius = 1.0 | units.RSun
 
-    earth = stars[1]
+    earth = bodies[1]
     earth.mass = 5.9736e24 | units.kg
-    earth.radius = 6371.0 | units.km
     earth.position = (149.5e6, 0.0, 0.0) | units.km
     earth.velocity = (0.0, 29800, 0.0) | (units.m / units.s)
+    earth.radius = 6371.0 | units.km
 
-    return stars
+    return bodies
 
+
+def twobodies_circular(mass1, mass2, sma):
+    """
+    Sets up a two-body system with zero eccentricity.
+   
+    mass1 : mass of central body
+    mass2 : mass of orbiting body
+    sma : semi-major-axis of the orbit, so here it's just the radius
+          of the circular orbit.
+ 
+    Returns
+    -------
+    Particles instance
+
+    """
+    bodies = Particles(2)
+
+    totalmass = mass1+mass2 
+    period = np.sqrt(4.0*numpy.pi**2*sma**3/(constants.G*(totalmass)))
+    angular_frequency = 2.0*numpy.pi / period
+    velocity = angular_frequency * sma
+
+    position_orbiting_body = [sma.value_in(sma.unit), 0, 0] | sma.unit
+    velocity_orbiting_body =  [velocity.value_in(velocity.unit), 0, 0] | velocity.unit
+
+    central = bodies[0]
+    central.mass = mass1
+    central.position = (0.0, 0.0, 0.0) | units.AU
+    central.velocity = (0.0, 0.0, 0.0) | (units.AU / units.s)
+
+    orbitingbody = bodies[1]
+    orbitingbody.mass = mass2 
+    orbitingbody.position = position_orbiting_body 
+    orbitingbody.velocity = velocity_orbiting_body 
+
+    return bodies
 
 
