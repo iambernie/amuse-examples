7c1a1ccebf76c1a7ce46aed3ee564aab281b14f2
iambernie <iambernie@gmail.com>
2014-04-01 00:26:52 +0200

    pep8styling

--- sim_binary_system_with_massloss.py
+++ sim_binary_system_with_massloss.py
@@ -24,8 +24,8 @@ def main():
     /
     ├── system_00
     │   ├── sequence_00
-    │   │   ├── center_of_mass
-    │   │   ├── center_of_mass_velocity
+    │   │   ├── CM_position
+    │   │   ├── CM_velocity
     │   │   ├── kinetic_energy
     │   │   ├── mass
     │   │   ├── position
@@ -51,7 +51,8 @@ def main():
 
     assert M>m
 
-    init_twobodies = [twobodies_circular(i|u.MSun, o|u.MSun, seperation|u.AU) for i in range(m, M)]
+    init_twobodies = [twobodies_circular(i|u.MSun, o|u.MSun, seperation|u.AU) 
+                      for i in range(m, M)]
 
     with HDF5HandlerAmuse(args.filename) as datahandler:
 
@@ -64,11 +65,15 @@ def main():
                 seq_str = "sequence_"+str(seqcount).zfill(2)
                 h5path = "/"+sys_str+"/"+seq_str+"/"
 
-                mass_sequence, time_sequence = massloss_evolution(time, bodies[0].mass, step=step)
-                assert len(time_sequence) == len(mass_sequence)
-                print(h5path, time.in_(u.yr), "totalsteps {}".format(len(mass_sequence)))
+                mass_seq, time_seq = massloss_evolution(time, bodies[0].mass, 
+                                                        step=step)
 
-                evolve_system_with_massloss(bodies, mass_sequence, time_sequence, datahandler, h5path)
+                print(h5path, time.in_(u.yr), 
+                      "totalsteps {}".format(len(mass_seq)))
+
+                evolve_system_with_massloss(bodies, mass_seq, time_seq, 
+                                            datahandler, h5path)
+
                 datahandler.file.flush()
 
 
@@ -81,12 +86,19 @@ def massloss_evolution(endtime, initmass, mf=0.5, step=2.0|u.day):
 
     """
     assert endtime > 2*step
-    timerange = numpy.arange(0, endtime.number, step.value_in(endtime.unit)) | endtime.unit
-    massrange = numpy.linspace(initmass.number, mf*initmass.number, len(timerange)) | initmass.unit
+
+    timerange = numpy.arange(0, endtime.number, 
+                             step.value_in(endtime.unit)) | endtime.unit
+
+    massrange = numpy.linspace(initmass.number, mf*initmass.number, 
+                               len(timerange)) | initmass.unit
+
+    assert len(timerange) == len(massrange)
     return massrange, timerange
 
 
-def evolve_system_with_massloss(particles, mass_sequence, time_sequence, datahandler, h5path):
+def evolve_system_with_massloss(particles, mass_sequence, time_sequence, 
+                                datahandler, h5path):
     """
 
     Parameters
@@ -98,41 +110,59 @@ def evolve_system_with_massloss(particles, mass_sequence, time_sequence, datahan
     h5path: unix-style path for this simulation's dataset
 
     """
-    integrator = Hermite(nbody_to_si(particles.total_mass(), 1 | u.AU))
-    integrator.particles.add_particles(particles)
+    h = datahandler
 
-    datahandler.append(particles[0].period0, h5path+"period0")
+    intr = Hermite(nbody_to_si(particles.total_mass(), 1 | u.AU))
+    intr.particles.add_particles(particles)
+
+    h.append(particles[0].period0, h5path+"period0")
 
     for mass, time in zip(mass_sequence, time_sequence):
-        integrator.particles.move_to_center() 
-        integrator.evolve_model(time)
-        integrator.particles[0].mass = mass 
 
-        datahandler.append(integrator.particles.center_of_mass(), h5path+"center_of_mass")
-        datahandler.append(integrator.particles.center_of_mass_velocity(), h5path+"center_of_mass_velocity")
-        datahandler.append(integrator.particles.position, h5path+"position")
-        datahandler.append(integrator.particles.velocity, h5path+"velocity")
-        datahandler.append(integrator.particles.mass, h5path+"mass")
-        datahandler.append(integrator.particles.kinetic_energy(), h5path+"kinetic_energy")
-        datahandler.append(integrator.particles.potential_energy(), h5path+"potential_energy")
-        datahandler.append(integrator.get_total_energy(), h5path+"total_energy")
-        datahandler.append(time, h5path+"time")
-        datahandler.append(semimajoraxis_from_binary(integrator.particles), h5path+"sma")
-        datahandler.append(eccentricity_from_binary(integrator.particles), h5path+"eccentricity")
+        intr.particles.move_to_center() 
+        intr.evolve_model(time)
+        intr.particles[0].mass = mass 
+
+        h.append(intr.particles.center_of_mass(), h5path+"CM_position")
+        h.append(intr.particles.center_of_mass_velocity(), h5path+"CM_velocity")
+        h.append(intr.particles.position, h5path+"position")
+        h.append(intr.particles.velocity, h5path+"velocity")
+        h.append(intr.particles.mass, h5path+"mass")
+        h.append(intr.particles.kinetic_energy(), h5path+"kinetic_energy")
+        h.append(intr.particles.potential_energy(), h5path+"potential_energy")
+        h.append(intr.get_total_energy(), h5path+"total_energy")
+        h.append(time, h5path+"time")
+        h.append(semimajoraxis_from_binary(intr.particles), h5path+"sma")
+        h.append(eccentricity_from_binary(intr.particles), h5path+"eccentricity")
      
-    integrator.stop()
+    intr.stop()
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--filename', required=True, metavar="HDF5 FILENAME")
-    parser.add_argument('-r','--resolution', type=int, default=4)
-    parser.add_argument('--maxmass', type=int, default=3, help="Max mass of central body in MSun")
-    parser.add_argument('--minmass', type=int, default=1, help="Min mass of central body in MSun")
-    parser.add_argument('--orbitmass', type=float, default=1, help="Mass of the orbiting body in MSun")
-    parser.add_argument('--seperation', type=float, default=1, help="Initial seperation in AU")
-    parser.add_argument('-s','--stepsize', type=float, metavar="IN_DAYS", default=2)
+
+    parser.add_argument('-f','--filename', required=True, 
+                        help="Filepath for hdf5 file.")
+
+    parser.add_argument('-r','--resolution', type=int, default=4, metavar="4")
+
+    parser.add_argument('--maxmass', type=int, default=3, metavar="3",
+                        help="Max mass of central body in MSun")
+
+    parser.add_argument('--minmass', type=int, default=1, metavar="1",
+                        help="Min mass of central body in MSun")
+
+    parser.add_argument('--orbitmass', type=float, default=0.1, metavar="0.1",
+                        help="Mass of the orbiting body in MSun")
+
+    parser.add_argument('--seperation', type=float, default=1, metavar="1",
+                        help="Initial seperation in AU")
+
+    parser.add_argument('-s','--stepsize', type=float, metavar="2", 
+                        default=2, help="Update mass ever -s days.")
+
     parser.add_argument('-t','--deltat', type=float,  default=3)
+
     args = parser.parse_args()
     return args
 
