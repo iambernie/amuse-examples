5c62a97a57b20f89806d4602d6f1bf52bda86ebe
iambernie <iambernie@gmail.com>
2014-04-14 13:56:39 +0200

    added inspecthdf5.py

---
+++ inspecthdf5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+import h5py
+from ext.misc import printdset
+
+def main():
+    f = h5py.File(args.filename, 'r')
+    f.visititems(printdset)
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--filename', required=True,
+                        help="hdf5 file created by sim_veras_multiplanet.py")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main()
+
