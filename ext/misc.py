1e112a84c6f86f007eeab75b4d6485dc850b7436
iambernie <iambernie@gmail.com>
2014-05-11 17:16:10 +0200

    comments

--- ext/misc.py
+++ ext/misc.py
@@ -17,31 +17,35 @@ from amuse.ext import orbital_elements as orbital_elements_amuse
 import progressbar as pb
 
 def args_quantify(unit):
+    """ 
+    Returns an argparse.Action to parse cmdline arguments as AMUSE quantities.
+
+    """
     class QuantifyAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
             quantity = values | unit
             setattr(namespace, self.dest, quantity)
+
     return QuantifyAction
 
+
+
 def args_integrators():
-    valid_integrators = dict(Mercury=Mercury, Hermite=Hermite, ph4=ph4, Huayno=Huayno,
-                             SmallN=SmallN)
+    valid_integrators = dict(Mercury=Mercury, Hermite=Hermite, ph4=ph4, 
+                             Huayno=Huayno, SmallN=SmallN)
 
     class IntegratorsAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
-           
             integrators = []
-
             for name in values: 
                 if name in valid_integrators:
                     integrators.append(valid_integrators[name])
                 else:
                     raise Exception('Invalid integrator: {}'.format(name))
-                
             setattr(namespace, self.dest, integrators)
+
     return IntegratorsAction
     
-
 
 class Parameters(object):
     def __init__(self, hdf5filename):
